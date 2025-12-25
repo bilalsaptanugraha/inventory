7a5d231cf03703ac896043fd4c91aa25998c3805
bilalsaptanugraha <bilalsapta23@if.unjani.ac.id>
2025-12-25 15:18:02 +0700

    perubahan tampilan

--- stock/notes.php
+++ stock/notes.php
@@ -1,42 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Notes</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-
 <?php
 session_start();
 include_once '../dbconnect.php';
 
-$res=mysqli_query($conn,"SELECT * FROM login WHERE id".$_SESSION['id']);
-
-$konten = $_POST['konten'];
-$oleh = $_SESSION['user'];
-
-$update = "INSERT INTO notes (contents, admin) VALUES ('$konten','$oleh')";
-$hasil = mysqli_query($conn,$update);
-
-
-if ($hasil){
-//header ('location:view.php');
-echo " <div class='alert alert-success'>
-    <strong>Success!</strong> Redirecting you back in 1 seconds.
-  </div>
-<meta http-equiv='refresh' content='1; url= index.php'/>  ";
-} else { echo "<div class='alert alert-warning'>
-    <strong>Failed!</strong> Redirecting you back in 1 seconds.
-  </div>
- <meta http-equiv='refresh' content='1; url= index.php'/> ";
+// CEGAH AKSES LANGSUNG
+if (!isset($_POST['konten'])) {
+    header("location:index.php");
+    exit;
 }
-?>
 
-</body>
-</html>
+$konten = mysqli_real_escape_string($conn, $_POST['konten']);
+$oleh   = $_SESSION['user'];
+
+$query = "INSERT INTO notes (contents, admin, status) 
+          VALUES ('$konten', '$oleh', 'aktif')";
+
+$hasil = mysqli_query($conn, $query);
+
+if ($hasil) {
+    echo "
+    <div style='padding:20px; background:#d4edda; color:#155724'>
+        <b>Success!</b> Note berhasil ditambahkan.
+    </div>
+    <meta http-equiv='refresh' content='1; url=index.php'>
+    ";
+} else {
+    echo "
+    <div style='padding:20px; background:#f8d7da; color:#721c24'>
+        <b>Failed!</b> Gagal menambahkan note.
+    </div>
+    <meta http-equiv='refresh' content='2; url=index.php'>
+    ";
+}
