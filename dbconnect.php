4b198bcd69fb17c360808074f13d0befd807026f
bilalsaptanugraha <bilalsapta23@if.unjani.ac.id>
2025-12-25 18:14:28 +0700

    ignore file

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,33 @@
 <?php 
+
+function loadEnv($path) {
+    if (!file_exists($path)) {
+        throw new Exception(".env file not found");
+    }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (str_starts_with(trim($line), '#')) continue;
+
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+
+        putenv("$name=$value");
+        $_ENV[$name] = $value;
+        $_SERVER[$name] = $value;
+    }
+}
+
+loadEnv(__DIR__ . '/.env');
+
 // isi nama host, username mysql, dan password mysql anda
-$conn = mysqli_connect("localhost","root","","inventory");
+$host = getenv('DB_HOST');
+$user = getenv('DB_USERNAME');
+$pass = getenv('DB_PASSWORD');
+$db   = getenv('DB_NAME');
+
+
+$conn = mysqli_connect($host,$user,$pass,$db);
 
 ?>
