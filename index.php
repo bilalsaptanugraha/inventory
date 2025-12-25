cdbe690427561716e1ee154d5279fa8dc65719b4
bilalsaptanugraha <bilalsapta23@if.unjani.ac.id>
2025-12-25 15:13:09 +0700

    perubahan index

--- index.php
+++ index.php
@@ -3,121 +3,95 @@ session_start();
 include 'dbconnect.php';
 
 if(isset($_SESSION['role'])){
-	header("location:stock");
+    header("location:stock");
+    exit;
 }
 
 if(isset($_GET['pesan'])){
-		if($_GET['pesan'] == "gagal"){
-			echo "Username atau Password salah!";
-		}else if($_GET['pesan'] == "logout"){
-			echo "Anda berhasil keluar dari sistem";
-		}else if($_GET['pesan'] == "belum_login"){
-			echo "Anda harus Login";
-		}else if($_GET['pesan'] == "noaccess"){
-			echo "Akses Ditutup";
-	}
+    if($_GET['pesan'] == "gagal"){
+        echo "<script>alert('Username atau Password salah!');</script>";
+    }else if($_GET['pesan'] == "logout"){
+        echo "<script>alert('Anda berhasil keluar dari sistem');</script>";
+    }else if($_GET['pesan'] == "belum_login"){
+        echo "<script>alert('Anda harus login terlebih dahulu');</script>";
+    }else if($_GET['pesan'] == "noaccess"){
+        echo "<script>alert('Akses ditutup');</script>";
+    }
 }
 
+if(isset($_POST['btn-login'])){
+    $uname = mysqli_real_escape_string($conn, $_POST['username']);
+    $upass = mysqli_real_escape_string($conn, md5($_POST['password']));
 
-if(isset($_POST['btn-login']))
-{
- $uname = mysqli_real_escape_string($conn,$_POST['username']);
- $upass = mysqli_real_escape_string($conn,md5($_POST['password']));
+    $login = mysqli_query($conn,"SELECT * FROM slogin WHERE username='$uname' AND password='$upass'");
+    $cek = mysqli_num_rows($login);
 
- // menyeleksi data user dengan username dan password yang sesuai
-$login = mysqli_query($conn,"select * from slogin where username='$uname' and password='$upass';");
-// menghitung jumlah data yang ditemukan
-$cek = mysqli_num_rows($login);
- 
-// cek apakah username dan password di temukan pada database
-if($cek > 0){
- 
-	$data = mysqli_fetch_assoc($login);
- 
- if($data['role']=="stock"){
-		// buat session login dan username
-		$_SESSION['user'] = $data['nickname'];
-		$_SESSION['user_login'] = $data['username'];
-		$_SESSION['id'] = $data['id'];
-		$_SESSION['role'] = "stock";
-		header("location:stock");
+    if($cek > 0){
+        $data = mysqli_fetch_assoc($login);
 
- }
- else
- {
-  header("location:index.php?pesan=gagal");
+        if($data['role'] == "stock"){
+            $_SESSION['user'] = $data['nickname'];
+            $_SESSION['user_login'] = $data['username'];
+            $_SESSION['id'] = $data['id'];
+            $_SESSION['role'] = "stock";
+            header("location:stock");
+            exit;
+        }
+    }
 
- }
- 
+    header("location:index.php?pesan=gagal");
 }
-
-}
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="UTF-8">
     <title>System Login</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144808195-1"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	  gtag('config', 'UA-144808195-1');
-	</script>
-    <script src="jquery.min.js"></script>
-	<style>body{background-image:url("bg.jpg");}
-	@media screen and (max-width: 600px) {
-h4{font-size:85%;}
-}
-	</style>
-	<link rel="icon" 
-      type="image/png" 
-      href="favicon.png">
-  </head>
-  <body>
-  
-  <div align="center">
-  
-  
-  
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-  <img src="logo.png" width="50%" style="margin-top:5%" \>
+    <style>
+        body{
+            background-image: url("bg.jpg");
+            background-size: cover;
+            background-position: center;
+            min-height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
-	<br \><br \>
-			<div class="container">
-					<div style="color:white">
-					<label>Login information for Demo :</label><br \>
-					<label>Username & Password : guest</label><br \>
-					</div>
-                <form method="post">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Username" name="username" autofocus>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password" name="password">
-                    </div>
-                    <button type="submit" class="btn btn-primary" name="btn-login">Masuk</button>
-			
-                </form>
-			
-			<br \>
-        </div></div>
-       
-     
-	
-	
-  </body>
+        .login-box{
+            width: 100%;
+            max-width: 400px;
+        }
+    </style>
+
+    <link rel="icon" type="image/png" href="favicon.png">
+</head>
+
+<body>
+
+<div class="login-box">
+
+    <form method="post">
+        <div class="form-group">
+            <input type="text" name="username" class="form-control text-center" placeholder="Username" required autofocus>
+        </div>
+
+        <div class="form-group">
+            <input type="password" name="password" class="form-control text-center" placeholder="Password" required>
+        </div>
+
+        <button type="submit" name="btn-login" class="btn btn-primary btn-block">
+            Masuk
+        </button>
+    </form>
+
+</div>
+
+</body>
 </html>
